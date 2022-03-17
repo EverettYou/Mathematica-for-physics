032f656510fc3564246d01da436061e1a3075ebe
Everett <everettyou.internet@gmail.com>
2022-03-17 04:52:30 -0700

    update

---
+++ Themes/PacletInfo.m
@@ -0,0 +1,17 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+(* created 2016/05/28*)
+
+Paclet[
+    Name -> "Themes",
+    Version -> "0.0.1",
+    MathematicaVersion -> "10+",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
---
+++ Toolkit/PacletInfo.m
@@ -0,0 +1,17 @@
+(* ::Package:: *)
+
+(* Paclet Info File *)
+
+(* created 2013/04/26*)
+
+Paclet[
+    Name -> "Toolkit",
+    Version -> "0.0.1",
+    MathematicaVersion -> "8+",
+    Extensions -> 
+        {
+            {"Documentation", Language -> "English"}
+        }
+]
+
+
