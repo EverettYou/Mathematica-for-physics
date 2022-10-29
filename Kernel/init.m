c09666c536f75e96c4e9a5a2332cf3ad0e69aab9
Everett <EverettYou@users.noreply.github.com>
2022-10-28 23:32:45 -0700

    update CMU Article

--- Kernel/init.m
+++ Kernel/init.m
@@ -1,10 +1,9 @@
 (* ::Package:: *)
 
-(* ::Code::Initialization:: *)
 (** User Mathematica initialization file **)
 Needs["PauliAlgebra`"];
 Needs["LoopIntegrate`"];
 Needs["MatsubaraSum`"];
 Needs["DiagramEditor`"]; 
 Needs["Toolkit`"];
-Needs["Themes`"];
+Get["Themes`"];
